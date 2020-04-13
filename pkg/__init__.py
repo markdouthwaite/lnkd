078a8b7102d89b152031472b12001cb451848638
Mark Douthwaite <md637@york.ac.uk>
2020-04-13 13:13:49 +0100

    Initial commit

---
+++ pkg/__init__.py
@@ -0,0 +1,13 @@
+"""
+A nice new Python package.
+"""
+
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+
+# this will load the latest _installed_ version.
+__version__ = metadata.version("pkg")
+
+print("Hello, world")
---
+++ tests/test_hello_world.py
@@ -0,0 +1,4 @@
+def test_hello_world():
+    message = "Hello, world!"
+
+    assert message == "Hello, world!"
